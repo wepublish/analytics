147743648970a31e52d47cf54177a29f1e199745
Kamil <kamil@disruptivelabs.io>
2022-06-27 13:09:11 +0100

    check for window

--- dist/index.js
+++ dist/index.js
@@ -11,34 +11,38 @@ const matomoPlugin = () => {
     return {
         name: "wp-matomo-analytics",
         page: () => {
-            const peerElement = document.querySelector("#peer-element");
-            if (peerElement != null) {
-                const peerName = peerElement.dataset.peerName;
-                const peerArticleId = peerElement.dataset.peerArticleId;
-                if (!!(peerName && peerArticleId)) {
-                    tracker.trackPageView({
-                        href: window.location.href,
-                        data: [
-                            {
-                                peerName,
-                                peerArticleId,
-                            },
-                        ],
-                    });
+            if (typeof window !== "undefined") {
+                const peerElement = document.querySelector("#peer-element");
+                if (peerElement != null) {
+                    const peerName = peerElement.dataset.peerName;
+                    const peerArticleId = peerElement.dataset.peerArticleId;
+                    if (!!(peerName && peerArticleId)) {
+                        tracker.trackPageView({
+                            href: window.location.href,
+                            data: [
+                                {
+                                    peerName,
+                                    peerArticleId,
+                                },
+                            ],
+                        });
+                    }
                 }
             }
         },
         track: () => {
-            const peerElement = document.querySelector("#peer-element");
-            if (peerElement != null) {
-                const peerName = peerElement.dataset.peerName;
-                const peerArticleId = peerElement.dataset.peerArticleId;
-                if (!!(peerName && peerArticleId)) {
-                    tracker.trackEvent({
-                        href: window.location.href,
-                        category: `Peer ${peerName} article visited`,
-                        action: `Some action ${peerArticleId}`,
-                    });
+            if (typeof window !== "undefined") {
+                const peerElement = document.querySelector("#peer-element");
+                if (peerElement != null) {
+                    const peerName = peerElement.dataset.peerName;
+                    const peerArticleId = peerElement.dataset.peerArticleId;
+                    if (!!(peerName && peerArticleId)) {
+                        tracker.trackEvent({
+                            href: window.location.href,
+                            category: `Peer ${peerName} article visited`,
+                            action: `Some action ${peerArticleId}`,
+                        });
+                    }
                 }
             }
         },
--- index.ts
+++ index.ts
@@ -9,38 +9,40 @@ const matomoPlugin = () => {
   return {
     name: "wp-matomo-analytics",
     page: () => {
-      const peerElement = document.querySelector(
-        "#peer-element"
-      ) as HTMLElement | null;
-      if (peerElement != null) {
-        const peerName = peerElement.dataset.peerName;
-        const peerArticleId = peerElement.dataset.peerArticleId;
-        if (!!(peerName && peerArticleId)) {
-          tracker.trackPageView({
-            href: window.location.href,
-            data: [
-              {
-                peerName,
-                peerArticleId,
-              },
-            ],
-          } as types.TrackParams);
+      if (typeof window !== "undefined") {
+        const peerElement =
+          document.querySelector<HTMLElement>("#peer-element");
+        if (peerElement != null) {
+          const peerName = peerElement.dataset.peerName;
+          const peerArticleId = peerElement.dataset.peerArticleId;
+          if (!!(peerName && peerArticleId)) {
+            tracker.trackPageView({
+              href: window.location.href,
+              data: [
+                {
+                  peerName,
+                  peerArticleId,
+                },
+              ],
+            } as types.TrackParams);
+          }
         }
       }
     },
     track: () => {
-      const peerElement = document.querySelector(
-        "#peer-element"
-      ) as HTMLElement | null;
-      if (peerElement != null) {
-        const peerName = peerElement.dataset.peerName;
-        const peerArticleId = peerElement.dataset.peerArticleId;
-        if (!!(peerName && peerArticleId)) {
-          tracker.trackEvent({
-            href: window.location.href,
-            category: `Peer ${peerName} article visited`,
-            action: `Some action ${peerArticleId}`,
-          });
+      if (typeof window !== "undefined") {
+        const peerElement =
+          document.querySelector<HTMLElement>("#peer-element");
+        if (peerElement != null) {
+          const peerName = peerElement.dataset.peerName;
+          const peerArticleId = peerElement.dataset.peerArticleId;
+          if (!!(peerName && peerArticleId)) {
+            tracker.trackEvent({
+              href: window.location.href,
+              category: `Peer ${peerName} article visited`,
+              action: `Some action ${peerArticleId}`,
+            });
+          }
         }
       }
     },
